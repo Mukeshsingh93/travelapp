0bfdcfe380f04c40e53f2e7674823545cf5b2041
jeenaabhilash <jeena@softgates.ae>
2020-04-26 13:02:28 +0400

    Travel App

--- app/src/main/java/com/mahalakshmi/travel/MainActivity.kt
+++ app/src/main/java/com/mahalakshmi/travel/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Handler().postDelayed({
-            
+
             val inten = Intent(this@MainActivity, HomeActivity::class.java)
             startActivity(inten)
             finish()
